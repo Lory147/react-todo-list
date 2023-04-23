c79e2045acaad03697b4ea9e40062d1c0f9d122b
Lorenzo Galazzi <galazzilorenzo@gmail.com>
2023-04-23 17:40:57 +0200

    first commit

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -5,42 +5,41 @@ import { Grid, Toolbar, AppBar, Paper, Typography } from "@mui/material";
 import useTodoState from "./hooks/useTodoState";
 
 function TodoApp() {
-    const initialTodos = JSON.parse(window.localStorage.getItem("todos") || "[]");
-    const { todos, addTodo, removeTodo, toggleTodo, editTodo } = useTodoState(
-        initialTodos
-    );
+  const initialTodos = JSON.parse(window.localStorage.getItem("todos") || "[]");
+  const { todos, addTodo, removeTodo, toggleTodo, editTodo } =
+    useTodoState(initialTodos);
 
-    useEffect(() => {
-        window.localStorage.setItem("todos", JSON.stringify(todos));
-    }, [todos]);
+  useEffect(() => {
+    window.localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
-    return (
-        <Paper
-            style={{
-                padding: 0,
-                margin: 0,
-                height: "100vh",
-                backgroundColor: "#fafafa"
-            }}
-            elevation={0}
-        >
-            <AppBar color='primary' position='static' style={{ height: "64px" }}>
-                <Toolbar>
-                    <Typography color='inherit'>TODOS WITH HOOKS</Typography>
-                </Toolbar>
-            </AppBar>
-            <Grid container justify='center' style={{ marginTop: "1rem" }}>
-                <Grid item xs={11} md={8} lg={4}>
-                    <TodoForm addTodo={addTodo} />
-                    <TodoList
-                        todos={todos}
-                        removeTodo={removeTodo}
-                        toggleTodo={toggleTodo}
-                        editTodo={editTodo}
-                    />
-                </Grid>
-            </Grid>
-        </Paper>
-    );
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "#fafafa",
+      }}
+      elevation={0}
+    >
+      <AppBar color="primary" position="static" style={{ height: "64px" }}>
+        <Toolbar>
+          <Typography color="inherit">TODOS WITH HOOKS</Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container justify="center" style={{ marginTop: "1rem" }}>
+        <Grid item xs={12} md={8} lg={4}>
+          <TodoForm addTodo={addTodo} />
+          <TodoList
+            todos={todos}
+            removeTodo={removeTodo}
+            toggleTodo={toggleTodo}
+            editTodo={editTodo}
+          />
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 }
 export default TodoApp;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import { Paper } from '@mui/material'
-import { List } from '@mui/material'
-import { Divider } from '@mui/material'
-import Todo from './Todo'
+import React from "react";
+import { Paper } from "@mui/material";
+import { List } from "@mui/material";
+import { Divider } from "@mui/material";
+import Todo from "./Todo";
 
 export default function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
-    return (
-        <Paper>
-            <List>
-                {todos.map((todo, i) => (
-                    <>
-                        <Todo
-                            id={todo.id}
-                            task={todo.task}
-                            key={todo.id}
-                            completed={todo.completed}
-                            removeTodo={removeTodo} 
-                            toggleTodo={toggleTodo}
-                            editTodo={editTodo}
-                            />
-                       {i < todos.lenght &&  <Divider />}
-                    </>
-                ))}
-            </List>
-        </Paper>
-    );
+  return (
+    <List>
+      {todos.map((todo, i) => (
+        <>
+          <Paper>
+            <Todo
+              id={todo.id}
+              task={todo.task}
+              key={todo.id}
+              completed={todo.completed}
+              removeTodo={removeTodo}
+              toggleTodo={toggleTodo}
+              editTodo={editTodo}
+            />
+            {i < todos.lenght && <Divider />}
+          </Paper>
+        </>
+      ))}
+    </List>
+  );
 }
